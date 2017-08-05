bb905bba747875e42dd81d2ac77c1391753c8d3a
Bruno Lucas <bruno-lucas@msn.com>
2017-08-04 23:35:25 -0300

    Melhorias e add icons, iniciando conteudo das regras

--- js/script.js
+++ js/script.js
@@ -2,14 +2,26 @@ var btn = document.querySelector('.btn-mobile');
 var nav = document.querySelector('.nav');
 var action = false;
 
+function span(num){
+    return document.querySelector('.btn-mobile span:nth-child('+ num +')');
+}
+
 btn.addEventListener('click', btnMobile);
 
 function btnMobile(){
     if(!action){
-        nav.style.left = "0";
+        nav.style.left = 0;
+        span(1).style.transform = "translate(0px, 8px) rotate(225deg)";
+        span(2).style.opacity = 0;
+        span(3).style.transform = "translate(0px, -10px) rotate(-225deg)";
+        btn.style.borderRadius = "50%";
         action = true;
     }else{
         nav.style.left = "-100%";
+        span(1).style.transform = "translate(0px, 0px) rotate(0deg)";
+        span(2).style.opacity = 1;
+        span(3).style.transform = "translate(0px, 0px) rotate(0deg)";
+        btn.style.borderRadius = "0";
         action = false;
     }
 }
